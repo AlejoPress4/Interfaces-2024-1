27c356a8a30881f7e9ce682fa1cdb6ad2dd8a909
Alejo Preciado <alejandro.preciado.123@gmail.com>
2024-03-14 16:03:53 -0500

    Actualizado

---
+++ Semana 4 - Copy/resources/js/index.js
@@ -0,0 +1,14 @@
+import Slider from "./slider.js";
+
+class App {
+  static main() {
+    new Slider({
+      container: "body",
+      gallery: "./resources/data/gallery.json",
+      interval: 3000,
+      visibleTitle: true,
+      visibleDescription: true,
+    });
+  }
+}
+App.main();
